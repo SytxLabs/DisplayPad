214aa342a8330f609908c7c769561b7afbea7654
Erik05Master <egeyer40@gmail.com>
2024-10-17 21:14:01 +0200

    edit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DisplayPad.py',
-    version='1.0.0',
+    version='1.0.1',
     author='Sytxlabs',
     author_email='info@sytxlabs.eu',
     url='https://sytxlabs.eu',
